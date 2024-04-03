5f2cbe9450789b9f1164476ba532cb94bb5af077
DFBlok <s219392862@mandela.ac.za>
2024-04-03 11:23:13 +0200

    addded new project

---
+++ tailwind/c-app/app/CH2/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function CH2() {
+  return (
+    <>
+        <div className='bg-teal-800 text-white py-10'>   
+      <div className='w-[80%] mx-auto bg-red-600'>
+        <div className='w-[50%]'>
+                 <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ea, aspernatur, eligendi illo hic labore, esse mollitia perspiciatis libero vitae velit recusandae! Harum quam 
+          atque dolorum facere perspiciatis exercitationem iste.</h1>
+        </div>
+ 
+      </div>
+    </div>
+    <div className='py-6'>
+      <div className='w-[80%] mx-auto'>
+        <div className="w-[70%]">
+          <h2 className='text-3x1 mb-6'>Lorem ipsum dolor sit amet consectetur.</h2>
+          <p className='mb-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium vero fugiat veritatis! Eveniet placeat illum ipsum quas aspernatur 
+            quaerat accusantium, labore nam atque autem aperiam magni fugit iste quo. At!</p>
+          <p className='mb-2'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus odio itaque eos maxime blanditiis? Doloribus blanditiis harum quisquam 
+            aliquid aut ut nesciunt sed reiciendis. Obcaecati, culpa nobis! Repellendus, dolor quasi.
+          </p>
+          <p className='mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis assumenda animi minima adipisci necessitatibus facilis veniam cupiditate illum ut explicabo in, possimus iusto, hic molestiae facere aspernatur cum cumque magni.
+          </p>
+
+          <button className='bg-green-200 py-2 px-4 rounded-full uppercase text-sm font-semibold'>I want to learn</button>
+        </div>
+      </div>
+    </div>
+    </>
+
+    
+  )
+}
+
+export default CH2
