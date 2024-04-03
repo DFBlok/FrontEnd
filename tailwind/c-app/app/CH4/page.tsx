351d682dea8b7b833924f496f5e3987d60696062
DFBlok <s219392862@mandela.ac.za>
2024-04-03 16:07:05 +0200

    updates

---
+++ tailwind/c-app/app/CH4/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const CH4 = () => {
+    return (
+      <>
+      <header className='bg-[#23424a] text-white py-12'>
+        <div className='w-[80%] mx-auto p-5'>
+          <div className=' w-1/2'>
+            <h1 className='text-4xl font-semibold mb-5'>Responsive design don&apos;t have to be a stuggle</h1>
+            <p className='text-lg mb-2'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus saepe esse impedit dolorem repellat provident eligendi voluptates ipsum dicta
+               voluptate cupiditate vero incidunt deleniti illo, sed, beatae est alias in!
+            </p>
+            <button className="px-3 py-2 bg-purple-600 rounded-full uppercase font-semibold text-black" >
+              I want to learn
+            </button>
+          </div>
+        </div>  
+      </header> 
+      <section className='text-black'>
+        <div className='w-[80%] mx-auto p-5'>
+          <div className='flex gap-10'>
+              <div className='text-center p-5'>
+                <h1 className='text-purple-500 font-bold text-2xl mb-4'>Cheap</h1>
+                <p className='justify-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius hic dignissimos error nemo? Soluta error autem sapiente dignissimos in earum eos, ratione voluptatum quia? Excepturi aut minima aperiam vero quidem.</p>
+              </div>
+              <div className='text-center p-5'>
+                <h1 className='text-purple-500 font-bold text-2xl mb-4'>Quick</h1>
+                <p className='text-center'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque, ab corporis assumenda nisi officia. Saepe quo enim praesentium placeat eum odio perferendis similique porro autem quasi, ea sunt nam?
+                </p>
+              </div>
+              <div className='text-center p-5'>
+                <h1 className='text-purple-500 font-bold text-2xl mb-4'>Quality</h1>
+                <p className='text-center'>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde officiis quis commodi accusamus iste vero debitis saepe nam! Quos nisi perspiciatis dolore tempore! Unde doloribus repudiandae perferendis minima totam blanditiis.
+                </p>
+              </div>
+          </div>
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default CH4
