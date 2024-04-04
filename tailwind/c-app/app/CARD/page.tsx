6740cd6106c7c75546b36b299fd161fb449a7e10
DFBlok <s219392862@mandela.ac.za>
2024-04-04 15:21:25 +0200

    card section

---
+++ tailwind/c-app/app/CARD/page.tsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+const CARD = () => {
+  return (
+    <div className='bg-gray-500 py-20'>
+        <div className='w-[80%] mx-auto'>
+            <div className='flex gap-4'>
+                <div className='flex-initial '>
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src="asset/hero-img.jpg" alt="Card image" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">Card Title</div>
+                    <p className="text-gray-700 text-base">
+                    This is the content of the card. You can put any text or other elements here.
+                    </p>
+                </div>
+                <div className="px-6 py-4">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag2</span>
+        </div>
+                    </div>
+                </div>
+                <div className='flex-initial '>
+                    <div >
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="asset/hero-img.jpg" alt="Card image" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Card Title</div>
+                        <p className="text-gray-700 text-base">
+                        This is the content of the card. You can put any text or other elements here.
+                        </p>
+                    </div>
+                    <div className="px-6 py-4">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag2</span>
+            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex-initial '>
+                    <div >
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="asset/hero-img.jpg" alt="Card image" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Card Title</div>
+                        <p className="text-gray-700 text-base">
+                        This is the content of the card. You can put any text or other elements here.
+                        </p>
+                    </div>
+                    <div className="px-6 py-4">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag2</span>
+            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex-initial '>
+                    <div >
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="asset/hero-img.jpg" alt="Card image" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Card Title</div>
+                        <p className="text-gray-700 text-base">
+                        This is the content of the card. You can put any text or other elements here.
+                        </p>
+                    </div>
+                    <div className="px-6 py-4">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag2</span>
+            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex-initial '>
+                    <div >
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="asset/hero-img.jpg" alt="Card image" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Card Title</div>
+                        <p className="text-gray-700 text-base">
+                        This is the content of the card. You can put any text or other elements here.
+                        </p>
+                    </div>
+                    <div className="px-6 py-4">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag2</span>
+            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default CARD
