2e78805023e6120285ee2bd8c88510448dbf3e8d
DFBlok <s219392862@mandela.ac.za>
2024-04-05 18:22:57 +0200

    contact page

--- tailwind/PORTFOLIO/my-portfolio-app/app/Page4/page.tsx
+++ tailwind/PORTFOLIO/my-portfolio-app/app/Page4/page.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
-function Page4() {
+const Page4 =()=> {
   return (
-    <div>
-        This Contact Page
-    </div>
+    <>
+      <section className='bg-gray-200 text-white py-12 '>
+        <div className='w-[70%] mx-auto p-5 bg-gray-700 rounded-3xl'>
+          <h1 className='text3xl font-bold mb-8'>Contact Me </h1>
+          <div className='grid grid-cols-2 gap-4'>
+            <div>
+              <h2 className='text-xl font-bold mb-2'>Address</h2>
+              <p>123 Main Street</p>
+              <p>Port Elizabeth, 6059</p>
+            </div>
+            <div >
+                <h2 className='text-xl font-bold mb-2'>Phone</h2>
+                <p>00 788 667678 0</p>
+            </div>
+            <div>
+              <h2 className='text-xl font-bold mb-2'>Email</h2>
+              <p>info@example.com</p>
+            </div>
+            <div>
+              <h2 className='text-xl font-bold mb-2'>Social Media</h2>
+              <ul>
+                <li>Facebook: <a href='#'>example</a></li>
+                <li>Twitter: <a href='#'>example</a></li>
+                <li>Instagram: <a href='#'>example</a></li>
+              </ul>
+            </div>
+          </div>
+              
+        </div>
+
+      </section>
+
+    </>
   )
 }
 
