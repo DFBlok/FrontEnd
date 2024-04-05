2db4c8bb1d789dd38bb0e3e26fc1cdb1cd2c9315
DFBlok <s219392862@mandela.ac.za>
2024-04-05 17:08:32 +0200

    Home Page completed

--- tailwind/PORTFOLIO/my-portfolio-app/app/page.tsx
+++ tailwind/PORTFOLIO/my-portfolio-app/app/page.tsx
@@ -3,8 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>HELLO WORLD!!</h1>
-    
+      <header className="bg-gray-200 text-white py-12 h-full">
+        <div className="w-[80%] mx-auto p-5  item-center">
+          <div>
+            <h1 className="text-4xl font-semibold py-9 text-center">HELLO WORLD!!
+            </h1>
+            <p className="text-black text-center italic">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde nesciunt deserunt, aperiam temporibus consectetur voluptatem tempore eum? Culpa molestias totam, fugit non deserunt, a, praesentium reiciendis unde voluptates adipisci excepturi.
+            </p>
+          </div>
+        </div>
+      </header>
+
+
     </>
   );
 }
