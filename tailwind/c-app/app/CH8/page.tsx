51463a081308fb49858224ee59b08ef548e641e3
DFBlok <s219392862@mandela.ac.za>
2024-04-10 10:30:37 +0200

    ch8 section 2 added

--- tailwind/c-app/app/CH8/page.tsx
+++ tailwind/c-app/app/CH8/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./ui/Navbar";
 import SectionOne from "./ui/SectionOne";
+import SectionTwo from "./ui/SectionTwo";
 
 const CH8 = () => {
   return (
@@ -8,6 +9,7 @@ const CH8 = () => {
       <h1>This is page 8</h1>
       <Navbar />
       <SectionOne />
+      <SectionTwo />
     </div>
   );
 };
---
+++ tailwind/c-app/app/CH8/ui/SectionTwo.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const SectionTwo = () => {
+  return (
+    <div>
+      <h1>SectionTwo</h1>
+      <section>
+        <div className="bg-[#A25652] text-white">
+          <div className="w-[70%] mx-auto flex p-5 gap-5 text-center">
+            <div>
+              <h1 className="text-center mb-3 text-[#a59678]">Cheap</h1>
+              <p className="text-center text-sm">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt molestiae nihil esse, reiciendis dignissimos, quae et
+                non temporibus hic corporis ducimus corrupti. Molestias, ad
+                amet? Ad dolore ratione distinctio sit.
+              </p>
+            </div>
+            <div>
+              <h1 className="text-center mb-3 text-[#a59678]">Quality</h1>
+              <p className="text-center text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium perferendis, eveniet consequatur odit adipisci eum
+                dolor? Nemo pariatur molestias mollitia, ea temporibus illum rem
+                accusantium fugit quisquam doloremque, beatae quos?
+              </p>
+            </div>
+            <div>
+              <h1 className="text-center mb-3 text-[#a59678]">Business</h1>
+              <p className="text-center text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                voluptatum, veniam dicta quos praesentium quis expedita dolores,
+                accusamus nobis velit est perspiciatis placeat pariatur
+                laudantium, sapiente commodi eos nisi! Nam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SectionTwo;
