876872b6fefccb9cd7201a384a092d11d344f3e9
DFBlok <s219392862@mandela.ac.za>
2024-04-11 14:55:22 +0200

    Project page update

--- tailwind/PORTFOLIO/my-portfolio-app/app/Page3/page.tsx
+++ tailwind/PORTFOLIO/my-portfolio-app/app/Page3/page.tsx
@@ -1,46 +1,84 @@
-import { Container } from 'postcss'
-import React from 'react'
+import { Container } from "postcss";
+import React from "react";
+import Image from "next/image";
 
 function Page3() {
   return (
     <>
-      <section className='bg-gray-200'>
-  <div className='w-[70%] mx-auto flex p-5 items-center flex-col'>
+      <section className="bg-gray-200">
+        <div className="w-[70%] mx-auto flex p-5 items-center flex-col">
+          <div className="w-full flex justify-between">
+            <div className="w-1/2 border-2 p-2 border-pink-500">
+              <div className="border-2 border-black p-4 mb-2">
+                <h1 className="text-xl font-bold mb-2">Project 1</h1>
+                <p className="text-gray-700 mb-4">
+                  This is the description for Project 1.
+                </p>
+                <Image
+                  src="/asset/project.jpg"
+                  alt="project image"
+                  className="mb-4"
+                  width={200}
+                  height={100}
+                />
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                  View Details
+                </button>
+              </div>
+              <div className="border-2 border-red-600 p-4 mb-2">
+                <h1 className="text-xl font-bold mb-2">Project 2</h1>
+                <p className="text-gray-700 mb-4">
+                  This is the description for Project 1.
+                </p>
+                <Image
+                  src="/asset/project.jpg"
+                  alt="project image"
+                  className="mb-4"
+                  width={200}
+                  height={100}
+                />
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                  View Details
+                </button>
+              </div>
+            </div>
+            <div className="w-1/2 border-2 p-2 border-pink-500 ml-2">
+              <div className="border-2 border-black p-4 mb-2">
+                <h1 className="text-xl font-bold mb-2">Project 3</h1>
+                <p className="text-gray-700 mb-4">
+                  This is the description for Project 1.
+                </p>
+                <Image
+                  src="/asset/project.jpg"
+                  alt="project image"
+                  className="mb-4"
+                  width={200}
+                  height={100}
+                />
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                  View Details
+                </button>
+              </div>
+              <div className="border-2 border-red-600 p-4 ">
+                <h1 className="text-xl font-bold mb-2">Project 4</h1>
+                <p className="text-gray-700 mb-4">
+                  This is the description for Project 1.
+                </p>
+                <Image
+                  src="/asset/project.jpg"
+                  alt="project image"
+                  className="mb-4"
+                  width={200}
+                  height={100}
+                />
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                  View Details
+                </button>
+              </div>
+            </div>
+          </div>
 
-    <div className='w-full flex justify-between'>
-      <div className='w-1/2 border-2 p-2 border-pink-500'>
-        <div className='border-2 border-black p-4 mb-2'>
-        <h1 className="text-xl font-bold mb-2">Project 1</h1>
-          <p className="text-gray-700 mb-4">
-            This is the description for Project 1.
-          </p>
-          <img src="https://placehold.co/100x100" alt="project image" className="mb-4" />
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">View Details</button>
-        </div>
-        <div className='border-2 border-red-600'>
-        <h1 className="text-xl font-bold mb-2">Project 2</h1>
-          <p className="text-gray-700 mb-4">This is the description for Project 1.</p>
-          <img src="https://placehold.co/100x100" alt="project image" className="mb-4" />
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">View Details</button>
-        </div>
-      </div>
-      <div className='w-1/2 border-2 p-2 border-pink-500 ml-2'>
-        <div className='border-2 border-black p-4 mb-2'>
-        <h1 className="text-xl font-bold mb-2">Project 3</h1>
-          <p className="text-gray-700 mb-4">This is the description for Project 1.</p>
-          <img src="https://placehold.co/100x100" alt="project image" className="mb-4" />
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">View Details</button>
-        </div>
-        <div className='border-2 border-red-600'>
-        <h1 className="text-xl font-bold mb-2">Project 4</h1>
-          <p className="text-gray-700 mb-4">This is the description for Project 1.</p>
-          <img src="https://placehold.co/100x100" alt="project image" className="mb-4" />
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">View Details</button>
-        </div>
-      </div>
-    </div>
-
-   {/*  <div className='w-full flex justify-between mt-8'>
+          {/*  <div className='w-full flex justify-between mt-8'>
       <div className='w-1/2 border-2 p-2 border-pink-500'>
         <div className='border-2 border-black'>
           <h1>This is Project Page.</h1>
@@ -59,10 +97,10 @@ function Page3() {
       </div>
     </div>
  */}
-  </div>
-</section>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Page3
+export default Page3;
