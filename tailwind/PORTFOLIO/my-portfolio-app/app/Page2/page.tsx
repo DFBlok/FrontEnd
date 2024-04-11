36041719ba343cc7e69de48f792492f82d27e29e
DFBlok <s219392862@mandela.ac.za>
2024-04-11 14:22:07 +0200

    Home/About page update

--- tailwind/PORTFOLIO/my-portfolio-app/app/Page2/page.tsx
+++ tailwind/PORTFOLIO/my-portfolio-app/app/Page2/page.tsx
@@ -1,37 +1,78 @@
-import React from 'react'
+import React from "react";
 
 const Page2 = () => {
-  return(
+  return (
     <>
-        <section className='bg-gray-200'>
-          <div className='w-[70%] mx-auto p-5 items-center'>
-            <div className='items-center'>
-              <img src="https://placehold.co/600x400/png" alt="placeholder" />
-            </div >
-            <div className='mt-8 flex justify-between w-full'>
-              <div className='w-1/2'>
-                  <h2 className='text-center m-4'>Colum 1</h2>
-                  <ul>
-                    <li>Mock Data 1</li>
-                    <li>mock Data 1</li>
-                    <li>mock Data 1</li>
-                    <li>mock Data 1</li>
-                  </ul>
-              </div>
-              <div className='w-1/2'>
-                <h2 className='text-center m-4'>Column 2</h2>
-                <ul>
-                  <li>Mock Data 1</li>
-                  <li>mock Data 1</li>
-                  <li>mock Data 1</li>
-                  <li>mock Data 1</li>
-                </ul>
-              </div>
+      <section className="bg-gray-200 py-12">
+        <div className="w-[70%] mx-auto p-5 items-center">
+          <div>
+            <h1 className="text-center font-bold text-4xl">About me</h1>
+            <hr />
+            <p className="text-m text-gray-700 text-center py-5 m-3">
+              Here you will find more information about me, what I do, and my
+              current skills mostly in terms of programming and technology
+            </p>
+          </div>
+          <div className="flex gap-6 ">
+            <div className="bg-red-500 w-[80%] p-4">
+              <h2 className="text-3xl font-bold mb-3">Get to know me!</h2>
+              <p className="text-m text-gray-200 mb-3">
+                I&apos;m a <b>Frontend Focused Web Developer</b> building and
+                managing the Front-end of Websites and Web Applications that
+                leads to the success of the overall product. Check out some of
+                my work in the Projects section.
+              </p>
+              <p className="text-m text-gray-200 mb-3">
+                I also like sharing content related to the stuff that I have
+                learned over the years in Web Development so it can help other
+                people of the Dev Community. Feel free to Connect or Follow me
+                on my Linkedin and Instagram where I post useful content related
+                to Web Development and Programming
+              </p>
+              <p className="text-m text-gray-200 mb-3">
+                I&apos;m open to Job opportunities where I can contribute, learn
+                and grow. If you have a good opportunity that matches my skills
+                and experience then don&apos;t hesitate to contact me.
+              </p>
+              <button className="text-lg bg-purple-600 text-white px-8 py-2 my-5 rounded-xl font-semibold">
+                Contact
+              </button>
+            </div>
+            <div className="bg-green-500 w/2">
+              <h1 className="text-2xl font-bold mb-3 p-4">My skils</h1>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                Html
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                CSS
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                JavaScript
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                Tailwindcss
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                Node.js
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                SQL
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                C#
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                Python
+              </button>
+              <button className="bg-gray-300 p-2 rounded-xl text-m font-semibold m-2">
+                ASP.Net/MVC
+              </button>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Page2
+export default Page2;
--- tailwind/PORTFOLIO/my-portfolio-app/app/page.tsx
+++ tailwind/PORTFOLIO/my-portfolio-app/app/page.tsx
@@ -3,19 +3,32 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="bg-gray-200 text-white py-12 w-full max-h-screen">
-        <div className="w-[80%] mx-auto p-5  item-center">
-          <div>
-            <h1 className="text-4xl font-semibold py-9 text-center">HELLO WORLD!!
-            </h1>
-            <p className="text-black text-center italic">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde nesciunt deserunt, aperiam temporibus consectetur voluptatem tempore eum? Culpa molestias totam, fugit non deserunt, a, praesentium reiciendis unde voluptates adipisci excepturi.
-            </p>
+      <header className="bg-gray-200 text-white py-12 w-full">
+        <div className="max-w-7xl mx-auto px-4 md:px-8">
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="md:w-1/2">
+              <h1 className="text-4xl font-semibold text-red-400 py-3 md:py-0 md:text-left mb-4">
+                Hello, I&apos;m Duwayne Blok
+              </h1>
+              <p className="text-black text-center md:text-left italic">
+                I&apos;m a Software Developer based in South Africa, Gqeberha,
+                formerly known as Port Elizabeth.
+              </p>
+            </div>
+            <div className="md:w-1/2 md:ml-8">
+              <div className="w-full md:w-96 mx-auto">
+                <Image
+                  src="/asset/profile.jpg"
+                  alt="profile"
+                  width={400}
+                  height={300}
+                  className="rounded-full"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </header>
-
-
     </>
   );
 }
