7120a67cd889aa693504efe3dacde8582729aa4f
DFBlok <s219392862@mandela.ac.za>
2024-10-28 09:30:16 +0200

    estate website aded

--- tailwind/PORTFOLIO/my-portfolio-app/app/Page3/page.tsx
+++ tailwind/PORTFOLIO/my-portfolio-app/app/Page3/page.tsx
@@ -38,7 +38,7 @@ function Page3() {
                 styled with TailwindCSS, and featuring routing, icons,
                 notifications, and HTTP requests.
               </p>
-              <div className="flex gap-4">
+              <div className="flex flex-col sm:flex-row gap-4">
                 <a
                   href="https://bookstore-crud-7hiofn3e6-dfbloks-projects.vercel.app/"
                   target="_blank"
@@ -83,7 +83,7 @@ function Page3() {
                 tenetur quae fugit unde excepturi a sunt odio veritatis quod
                 rerum, impedit natus exercitationem beatae.
               </p>
-              <div className="flex gap-4">
+              <div className="flex flex-col sm:flex-row gap-4">
                 <a
                   href="https://landing-page-lgd0nbtum-dfbloks-projects.vercel.app"
                   target="_blank"
@@ -105,31 +105,51 @@ function Page3() {
               </div>
             </div>
           </div>
+          {/*   </div> */}
           {/*Project 3 */}
           <div className="flex flex-col md:flex-row gap-5 border p-10 mb-10 bg-gray-100 rounded-3xl shadow-lg">
             <div className="w-full md:w-1/2 p-4">
               <Image
-                src="https://picsum.photos/900/500"
-                alt="Project Image"
+                /* src="https://picsum.photos/900/500" */
+                src="/asset/estate-landing-page.png"
+                alt="img"
                 layout="responsive"
                 width={900}
-                height={500}
+                height={900}
                 className="rounded-md border-2 border-white shadow-lg transform transition-transform duration-300 hover:scale-105"
               />
             </div>
             <div className="w-full md:w-1/2 p-4 flex flex-col justify-center">
               <h1 className="text-3xl text-gray-800 font-semibold mb-5">
-                Title: <span className="text-blue-500">MERN Project</span>
+                Title:{" "}
+                <span className="text-blue-500">Real Estate Landing Page</span>
               </h1>
+
               <p className="text-gray-600 leading-relaxed mb-8 text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusamus recusandae aliquam delectus rem quos molestiae,
-                tenetur quae fugit unde excepturi a sunt odio veritatis quod
-                rerum, impedit natus exercitationem beatae.
+                A React-based frontend project using Vite for fast development,
+                styled with TailwindCSS, and featuring routing, icons,
+                notifications, and HTTP requests.
               </p>
-              <button className="bg-blue-500 text-white px-6 py-3 rounded-full shadow-lg hover:bg-red-500 transition-all duration-300">
-                Case Study
-              </button>
+              <div className="flex flex-col sm:flex-row gap-4">
+                <a
+                  href="https://bookstore-crud-7hiofn3e6-dfbloks-projects.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="bg-blue-500 text-white px-6 py-3 rounded-full shadow-lg hover:bg-blue-700 transition-all duration-300">
+                    View Website
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/DFBlok/React-Projects/tree/main/BookStoreProject-MERN"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="bg-gray-800 text-white px-6 py-3 rounded-full shadow-lg hover:bg-gray-600 transition-all duration-300">
+                    View GitHub Code
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
 
@@ -158,7 +178,7 @@ function Page3() {
               {/* <button className="bg-blue-500 text-white px-6 py-3 rounded-full shadow-lg hover:bg-red-500 transition-all duration-300">
                 Case Study
               </button> */}
-              <div className="flex gap-4">
+              <div className="flex flex-col sm:flex-row gap-4">
                 <a
                   href="https://your-website-link.com"
                   target="_blank"
